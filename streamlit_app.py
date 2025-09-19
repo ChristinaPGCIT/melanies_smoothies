f531ee903833dd7ff7f720c095c8aa467b47c61c
mwu029 <99863620+ChristinaPGCIT@users.noreply.github.com>
2025-09-19 18:27:16 +1200

    Update streamlit_app.py
    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import panda as pd
+import pandas as pd
 
 #-----------------------------------------------------------------------
 # Title and Intro
